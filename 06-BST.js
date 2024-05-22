ed477a594e6a9ae607c99ae97b4f25cb9fa5092c
Trade-Offf <trade_offf@163.com>
2024-05-22 15:49:46 +0800

    新增二叉搜索树删除功能

--- 06-BST.js
+++ 06-BST.js
@@ -17,11 +17,11 @@ export class BST {
     if (this.root === null) {
       this.root = newNode;
     } else {
-      this.insertNode(this.root, newNode);
+      this.#insertNode(this.root, newNode);
     }
   }
   // 基于递归判断大小插入节点
-  insertNode(node, newNode) {
+  #insertNode(node, newNode) {
     if (newNode.value < node.value) {
       if (node.left === null) {
         node.left = newNode;
@@ -37,19 +37,7 @@ export class BST {
     }
   }
 
-  // 中序遍历
-  inOrderTraverse(callback) {
-    this.#inOrderTraverseNode(this.root, callback);
-  }
-  #inOrderTraverseNode(node, callback) {
-    if (node !== null) {
-      this.#inOrderTraverseNode(node.left, callback);
-      callback(node.value);
-      this.#inOrderTraverseNode(node.right, callback);
-    }
-  }
-
-  // 先序遍历
+  // 先序遍历: 根节点 -> 左子树 -> 右子树
   preOrderTraverse(callback) {
     this.#preOrderTraverseNode(this.root, callback);
   }
@@ -61,7 +49,19 @@ export class BST {
     }
   }
 
-  // 后序遍历
+  // 中序遍历: 左子树 -> 根节点 -> 右子树
+  inOrderTraverse(callback) {
+    this.#inOrderTraverseNode(this.root, callback);
+  }
+  #inOrderTraverseNode(node, callback) {
+    if (node !== null) {
+      this.#inOrderTraverseNode(node.left, callback);
+      callback(node.value);
+      this.#inOrderTraverseNode(node.right, callback);
+    }
+  }
+
+  // 后序遍历: 左子树 -> 右子树 -> 根节点
   postOrderTraverse(callback) {
     this.#postOrderTraverseNode(this.root, callback);
   }
@@ -71,5 +71,84 @@ export class BST {
       this.#postOrderTraverseNode(node.right, callback);
       callback(node.value);
     }
+  }
+
+  min() {
+    return this.#minNode(this.root);
+  }
+  #minNode(node) {
+    let current = node;
+    while (current !== null && current.left !== null) {
+      current = current.left;
+    }
+    return current.value;
+  }
+  max() {
+    return this.#maxNode(this.root);
+  }
+  #maxNode(node) {
+    let current = node;
+    while (current !== null && current.right !== null) {
+      current = current.right;
+    }
+    return current.value;
+  }
+  // 搜索树中是否存在某个值
+  search(value) {
+    return this.#searchNode(this.root, value);
+  }
+  #searchNode(node, value) {
+    if (node === null) {
+      return false;
+    }
+    if (value < node.value) {
+      return this.#searchNode(node.left, value);
+    } else if (value > node.value) {
+      return this.#searchNode(node.right, value);
+    } else {
+      return true;
+    }
+  }
+
+  remove(value) {
+    this.root = this.#removeNode(this.root, value);
+  }
+  #removeNode(node, value) {
+    if (node === null) {
+      return null;
+    }
+    if (value < node.value) {
+      node.left = this.#removeNode(node.left, value);
+      return node;
+    } else if (value > node.value) {
+      node.right = this.#removeNode(node.right, value);
+      return node;
+    } else {
+      // 没有子节点，直接移除
+      if (node.left === null && node.right === null) {
+        node = null;
+        return node;
+      }
+      // 只有一个子节点的节点，将其子节点提升为当前节点的位置
+      if (node.left === null) {
+        node = node.right;
+        return node;
+      } else if (node.right === null) {
+        node = node.left;
+        return node;
+      }
+      // 移除有两个子节点的节点，找到右子树中的最小节点，将其替换为当前节点
+      const aux = this.#findMinNode(node.right);
+      node.value = aux.value;
+      node.right = this.#removeNode(node.right, aux.value);
+      return node;
+    }
+  }
+  #findMinNode(node) {
+    let current = node;
+    while (current !== null && current.left !== null) {
+      current = current.left;
+    }
+    return current;
   }
 }
